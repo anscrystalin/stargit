03e941efb1789dbec58d1e5edb24d0701d78731b
anscrystalin <46999033+anscrystalin@users.noreply.github.com>
2019-02-06 12:22:07 +0530

    Add files via upload
    app2

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request,redirect,session
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
+from instapy import InstaPy
+from instapy.util import smart_run
+from flask import session
 
 
 
 mysql = MySQL()
 app = Flask(__name__)
+app.secret_key = 'why would I tell you my secret key?'
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -13,6 +17,8 @@ app.config['MYSQL_DATABASE_PASSWORD'] = 'Super1capacitor'
 app.config['MYSQL_DATABASE_DB'] = 'bucketlist'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 mysql.init_app(app)
+
+
 
 
 @app.route('/')
@@ -24,10 +30,58 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
+@app.route('/showSignIn')
+def showSignIn():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('signin.html')
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+        # connect to mysql
+
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.callproc('sp_validateLogin', (_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]), _password):
+                 session['user'] = data[0][0]
+                 return redirect('/userHome')
+            else:
+                return render_template('error.html', error='Wrong Email address or Password.')
+        else:
+            return render_template('error.html', error='Wrong Email address or Password.')
+
+
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    finally:
+        cursor.close()
+        conn.close()
 
 @app.route('/signUp', methods=['POST', 'GET'])
-def SignUp():
-    try:
+def signUp():
+     try:
         _name = request.form['inputName']
         _email = request.form['inputEmail']
         _password = request.form['inputPassword']
@@ -35,27 +89,27 @@ def SignUp():
         # validate the received values
         if _name and _email and _password:
 
-            # All Good, let's call MySQL
+             # All Good, let's call MySQL
 
-            conn = mysql.connect()
-            cursor = conn.cursor()
-            _hashed_password = generate_password_hash(_password)
-            cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
-            data = cursor.fetchall()
-            if len(data) is 0:
+             conn = mysql.connect()
+             cursor = conn.cursor()
+             _hashed_password = generate_password_hash(_password)
+             cursor.callproc('sp_createUser', (_name, _email, _hashed_password))
+             data = cursor.fetchall()
+
+             if len(data) is 0:
                 conn.commit()
                 return json.dumps({'message': 'User created successfully !'})
-            else:
-                return json.dumps({'error': str(data[0])})
+             else:
+                 return json.dumps({'error': str(data[0])})
         else:
             return json.dumps({'html': '<span>Enter the required fields</span>'})
 
-    except Exception as e:
+     except Exception as e:
         return json.dumps({'error': str(e)})
 
-    finally:
-            cursor.close()
-            conn.close()
+     finally:
+        cursor.close()
+        conn.close()
 if __name__ == '__main__':
     app.run(port=5002)
-
